dad8ec829bb0183bbd745edefd40305f0e653312
leninnaranjo55 <leninnaranjo55@gmail.com>
2016-02-19 12:52:31 +0100

    respuestacontroller

--- api/controllers/RespuestaController.js
+++ api/controllers/RespuestaController.js
@@ -9,6 +9,15 @@ module.exports = {
 	alrespuesta: function(req, res, next) {
 		var respuestaRec = req.body.answered;
 		
+			
+		Opcion.findOne({
+			where: { id: Number(respuestaRec) }
+		}).then(function(opcion){
+			if(opcion){
+
+			}
+		}).catch(function(error){next(error);});
+		
 	}
 
 };
